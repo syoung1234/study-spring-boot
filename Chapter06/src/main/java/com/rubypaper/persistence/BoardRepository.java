d60b4bffa3edfc146dc23c006876df95d264a0c4
syoung1234 <syoung8818@gmail.com>
2022-10-26 14:54:52 +0900

    Chatper06

---
+++ Chapter06/src/main/java/com/rubypaper/persistence/BoardRepository.java
@@ -0,0 +1,9 @@
+package com.rubypaper.persistence;
+
+import com.rubypaper.domain.Board;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BoardRepository extends CrudRepository<Board, Long> {
+    
+}
