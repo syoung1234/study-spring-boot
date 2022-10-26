5a3426b3bc2c34ad6a69e14eae4f6323e3da7abf
syoung1234 <syoung8818@gmail.com>
2022-10-26 14:48:39 +0900

    Chapter01

---
+++ Chapter01/src/main/java/com/ruby/OtherController.java
@@ -0,0 +1,11 @@
+package com.ruby;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OtherController {
+    public OtherController() {
+        System.out.println("===> OtherController 생성");
+    }
+    
+}
